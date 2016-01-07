0e05f1e88d5f44191a8b8c4c5170737d1ace9282
Chris Parsons <chris.parsons@graphitedigital.com>
2016-01-07 15:32:33 +0000

    site files

---
+++ js/main.js
@@ -0,0 +1,58 @@
+function onScrollInit( items, trigger ) {
+  items.each( function() {
+    var osElement = $(this),
+        osAnimationClass = osElement.attr('data-os-animation'),
+        osAnimationDelay = osElement.attr('data-os-animation-delay');
+      
+        osElement.css({
+          '-webkit-animation-delay':  osAnimationDelay,
+          '-moz-animation-delay':     osAnimationDelay,
+          'animation-delay':          osAnimationDelay
+        });
+
+        var osTrigger = ( trigger ) ? trigger : osElement;
+        
+        osTrigger.waypoint(function() {
+          osElement.addClass('animated').addClass(osAnimationClass);
+          },{
+              triggerOnce: true,
+              offset: '90%'
+        });
+  });
+}
+
+onScrollInit( $('.os-animation') );
+onScrollInit( $('.staggered-animation'), $('.staggered-animation-container') );
+
+
+$(function() {
+    //caches a jQuery object containing the header element
+    var header = $(".site_header");
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 500) {
+            header.addClass("fixed fadeInDown");
+        } else {
+            header.removeClass("fixed fadeInDown");
+        }
+    });
+});
+
+
+
+// Smooth scroll - Click anchor tag to smoothly scroll you to the relavant section
+$(function() {
+  	$('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	});
+});
